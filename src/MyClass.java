0c89fd216f9b211d6dd1e89911b9cccd7572268a
inji <iij0573@naver.com>
2021-06-21 20:56:23 +0900

    First commit in dev branch

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,6 +3,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("hello git!");
 		System.out.println("this is 2nd version");
-		new MyHello().sayHello();
+		MyHello myHello = new MyHello();
+		myHello.sayHello();
 	}
 }
--- src/MyHello.java
+++ src/MyHello.java
@@ -1,6 +1,8 @@
 
 public class MyHello {
 	public void sayHello() {
-		System.out.println("hello");
+		System.out.println("hello1");
+		System.out.println("hello2");
+		System.out.println("hello3");
 	}
 }
